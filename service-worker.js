4c0580458b5a1c2f9cdac35581735ab20687520b
Sergio Alcántara Romero <seya.28@gmail.com>
2026-09-22 08:38:37 +0200

    Actualizar service-worker.js para la nueva receta

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='dulce-a-tu-ritmo-v8';
+const CACHE_NAME='dulce-a-tu-ritmo-v9';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-any.svg','./assets/process-sprite.jpg','./assets/rollos/enrollado.jpg','./assets/rollos/segundo-levado.jpg','./assets/rollos/resultado.jpg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
